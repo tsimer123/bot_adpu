cca1a95f4a04c89d34acd6a00c95b66491c27b20
uskondratiev <uskondratiev@gmail.com>
2024-09-02 15:02:26 +0300

    emetr correct

--- bot/handlers/emeter.py
+++ bot/handlers/emeter.py
@@ -3,6 +3,8 @@ import smtplib
 import pandas as pd
 import xlsxwriter
 import datetime
+import re
+from email.utils import parseaddr
 from email.message import EmailMessage
 from dotenv import load_dotenv
 from aiogram import types
@@ -27,24 +29,34 @@ async def command_emeter(message: types.Message) -> None:
     except Exception as ex:
         print_format_log_cmd(list_param_log_cmd, 'err', ex.args[0])
         await message.reply('Ошибка Базы Данных (code error: 1003).\n Обратитесь к Администратору @etsimerman')
-    string = (message.text).split()
     msg = EmailMessage()
     msg['Subject'] = 'meter'
     msg['From'] = login
     msg.set_content('See attached file')
-    msg['To'] = string[1]
-    try:
-        conn = db.connect()
-        sql_query = pd.read_sql("SELECT id, user_id, username, first_name, last_name, number_meter, imei, iccid1, iccid2, latitude, longitude, number_task, montag, power, created_on, state_meter FROM meter order by id;", con=conn)
-        df = pd.DataFrame(sql_query, columns = ['id', 'user_id', 'username', 'first_name', 'last_name', 'number_meter', 'imei', 'iccid1', 'iccid2', 'latitude','longitude', 'number_task', 'montag', 'power', 'created_on', 'state_meter'])   
-    except Exception as ex:
-        print_format_log_cmd(list_param_log_cmd, 'err', ex.args[0])
-        return await message.reply("Сервис временно не доступен. Ошибка базы.")
-    finally:
-        conn.close()
-    filename = "meter {}.xlsx".format(datetime.date.today().strftime("%d.%m.%y"))
-    msg.add_attachment(fit(df), maintype='application', subtype='vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename=filename)
-    await message.reply(send_mail_smtp(msg, host, login, password, message))
+    string = (message.text).split()
+    if len(string) == 2:
+        address = string[1]
+        email_regex = re.compile((r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"))
+        is_valid = email_regex.fullmatch(address) 
+        parsed_email = parseaddr(address)[1] 
+        if (is_valid is not None and parsed_email == address):
+            msg['To'] = string[1]
+            try:
+                conn = db.connect()
+                sql_query = pd.read_sql("SELECT id, user_id, username, first_name, last_name, number_meter, imei, iccid1, iccid2, latitude, longitude, number_task, montag, power, created_on, state_meter FROM meter order by id;", con=conn)
+                df = pd.DataFrame(sql_query, columns = ['id', 'user_id', 'username', 'first_name', 'last_name', 'number_meter', 'imei', 'iccid1', 'iccid2', 'latitude','longitude', 'number_task', 'montag', 'power', 'created_on', 'state_meter'])   
+            except Exception as ex:
+                print_format_log_cmd(list_param_log_cmd, 'err', ex.args[0])
+                return await message.reply("Сервис временно не доступен. Ошибка базы.")
+            finally:
+                conn.close()
+            filename = "meter {}.xlsx".format(datetime.date.today().strftime("%d.%m.%y"))
+            msg.add_attachment(fit(df), maintype='application', subtype='vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename=filename)
+            await message.reply(send_mail_smtp(msg, host, login, password, message))
+        else:
+            return await message.reply("Электронный адрес не соответствует стандартам RFC-822. Введите корректные данные (/emeter user@example.com)")
+    else:
+        return await message.reply("Введите корректные данные (/emeter user@example.com)")
 
 def send_mail_smtp(mail, host, username, password, message):
     try:
