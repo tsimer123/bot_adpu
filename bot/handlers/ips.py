8eb85e7f5b9f98c2aa6b4a505f332cc1f1e23aae
uskondratiev <uskondratiev@gmail.com>
2025-06-27 15:14:46 +0300

    add ips

--- bot/handlers/ips.py
+++ bot/handlers/ips.py
@@ -7,20 +7,20 @@ from bot.create_bot import dp, bot
 from sql.engine import engine
 
 db = engine
-async def command_ip(message: types.Message) -> None:
+async def command_ips(message: types.Message) -> None:
     output = io.BytesIO()
-    df3 = pd.DataFrame({'operator': [], 'iccid': [], 'number_tel': [], 'ip': [], 'apn': [], 'imei': []})
+    df3 = pd.DataFrame({'operator': [], 'iccid': [], 'msisdn': [], 'ip': [], 'apn': [], 'apnusername': [], 'password': []})
     string = message.text
     for ip in find_ips_ipaddress(string):    
         conn = db.connect() 
-        sql_query = pd.read_sql("select * from sims where ip = %(ip)s;", con=conn, params={'ip': ip})
-        df = pd.DataFrame(sql_query, columns = ['operator', 'iccid', 'number_tel', 'ip', 'apn', 'imei'])
+        sql_query = pd.read_sql("select * from simcards where ip = %(ip)s;", con=conn, params={'ip': ip})
+        df = pd.DataFrame(sql_query, columns = ['operator', 'iccid', 'msisdn', 'ip', 'apn', 'apnusername', 'password'])
         conn.close()
         num_rows = df.shape[0]
         if num_rows > 0 :
             df3 = pd.concat([df3, df], ignore_index=True)
         else :
-            df3.loc[len(df3.index)] = ['нет данных', 'нет данных', 'нет данных', ip, 'нет данных', 'нет данных']
+            df3.loc[len(df3.index)] = ['нет данных', 'нет данных', 'нет данных', ip, 'нет данных', 'нет данных', 'нет данных']
     num_rows = df3.shape[0] 
     if num_rows > 0 and num_rows < 2 :
         operator = df3['operator'].squeeze()
@@ -28,14 +28,16 @@ async def command_ip(message: types.Message) -> None:
         msisdn = df3['msisdn'].squeeze()
         ip = df3['ip'].squeeze()
         apnname = df3['apn'].squeeze()
-        imei = df3['imei'].squeeze()
+        apnusername = df3['apnusername'].squeeze()
+        password = df3['password'].squeeze()
         await message.reply(
             f"<b>operator:</b> {operator}\n"
             f"<b>iccid:</b> {iccid}\n"
             f"<b>msisdn:</b> {msisdn}\n"
             f"<b>ip:</b> {ip}\n"
             f"<b>apname:</b> {apnname}\n"
-            f"<b>imei:</b> {imei}\n",
+            f"<b>apnusername:</b> {apnusername}\n"    
+            f"<b>password:</b> {password}\n",
             parse_mode = 'HTML'                   
         )
     if num_rows > 1 :
